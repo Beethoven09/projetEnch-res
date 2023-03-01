fd682681609caee2db9dd53996613de6595d2e7e
Sébastien <srouillon@icloud.com>
2023-03-01 18:35:59 +0100

    MAJ - éclaircissement du projet dans la branche

--- src/main/java/fr/eni/dal/SQLController.java
+++ src/main/java/fr/eni/dal/SQLController.java
@@ -1,13 +1,9 @@
 package fr.eni.dal;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Base64;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -73,36 +69,4 @@ public class SQLController {
         return true;
     }
     
-	public Utilisateur insertUtilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue, int cp, String ville, String password, int credit, int administrateur) throws SQLException {
-		
-    	
-    	Utilisateur user = null;
-    	SecureRandom srnd = new SecureRandom();
-    	byte[] salt = new byte[16];
-    	srnd.nextBytes(salt);
-    	String base64salt = Base64.getEncoder().encodeToString(salt);
-    	String hashedPassword = hashPassword(password, base64salt);
-    	
-    	try (Connection conn = dataSource.getConnection()) {
-    		PreparedStatement stmt = conn.prepareStatement(INSERT, PreparedStatement.RETURN_GENERATED_KEYS);
-    		stmt.setString(1, pseudo);
-    		stmt.setString(2, nom);
-    		stmt.setString(3, prenom);
-    		stmt.setString(4, email);
-    		stmt.setString(5, telephone);
-    		stmt.setString(6, rue);
-    		stmt.setInt(7, cp);
-    		stmt.setString(8, ville);
-    		stmt.setString(9, hashedPassword);
-    		stmt.setInt(10, credit);
-    		stmt.setInt(11, administrateur);
-    		stmt.executeUpdate();
-    		ResultSet rs = stmt.getGeneratedKeys();
-    		if (rs.next()) {
-				int id = rs.getInt(1);
-				user = new Utilisateur(id, pseudo, nom, prenom, email, telephone, rue, cp, ville, credit, administrateur);
-			}
-        }
-        return user;
-    }
 }
