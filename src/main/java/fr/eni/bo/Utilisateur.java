4b05ad9fbc2fd6129cda0845675508a9bbaefc1c
Sébastien <srouillon@icloud.com>
2023-03-06 16:52:51 +0100

    init commit

--- src/main/java/fr/eni/bo/Utilisateur.java
+++ src/main/java/fr/eni/bo/Utilisateur.java
@@ -1,22 +1,22 @@
 package fr.eni.bo;
 
 public class Utilisateur {
-	private int id;
-	private String pseudo;
-	private String nom;
-	private String prenom;
-	private String email;
-	private String telephone;
-	private String rue;
-	private int cp;
-	private String ville;
-	private String password;
-	private int credit;
-	private int administrateur;
-	
-	
-	/**
-	 * @param id
+    private int id;
+    private String pseudo;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String telephone;
+    private String rue;
+    private int cp;
+    private String ville;
+    private String password;
+    private int credit;
+    private int administrateur;
+
+    
+
+    /**
 	 * @param pseudo
 	 * @param nom
 	 * @param prenom
@@ -25,12 +25,12 @@ public class Utilisateur {
 	 * @param rue
 	 * @param cp
 	 * @param ville
+	 * @param password
 	 * @param credit
 	 * @param administrateur
 	 */
-	public Utilisateur(int id, String pseudo, String nom, String prenom, String email, String telephone, String rue,
-			int cp, String ville, int credit, int administrateur) {
-		this.id = id;
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue, int cp,
+			String ville, String password, int credit, int administrateur) {
 		this.pseudo = pseudo;
 		this.nom = nom;
 		this.prenom = prenom;
@@ -39,151 +39,127 @@ public class Utilisateur {
 		this.rue = rue;
 		this.cp = cp;
 		this.ville = ville;
+		this.password = password;
 		this.credit = credit;
 		this.administrateur = administrateur;
 	}
-	/**
-	 * @return the id
-	 */
+
+	public Utilisateur(int id, String pseudo, String nom, String prenom, String email, String telephone, String rue, int cp, String ville, int credit, int administrateur, String password) {
+        this.id = id;
+        this.pseudo = pseudo;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.telephone = telephone;
+        this.rue = rue;
+        this.cp = cp;
+        this.ville = ville;
+        this.password = password;
+        this.credit = credit;
+        this.administrateur = administrateur;
+    }
+
+    // Getters et Setters 
+
+    public Utilisateur() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public int getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(int id) {
-		this.id = id;
-	}
-	/**
-	 * @return the pseudo
-	 */
-	public String getPseudo() {
-		return pseudo;
-	}
-	/**
-	 * @param pseudo the pseudo to set
-	 */
-	public void setPseudo(String pseudo) {
-		this.pseudo = pseudo;
-	}
-	/**
-	 * @return the nom
-	 */
-	public String getNom() {
-		return nom;
-	}
-	/**
-	 * @param nom the nom to set
-	 */
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	/**
-	 * @return the prenom
-	 */
-	public String getPrenom() {
-		return prenom;
-	}
-	/**
-	 * @param prenom the prenom to set
-	 */
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	/**
-	 * @return the telephone
-	 */
-	public String getTelephone() {
-		return telephone;
-	}
-	/**
-	 * @param telephone the telephone to set
-	 */
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-	/**
-	 * @return the rue
-	 */
-	public String getRue() {
-		return rue;
-	}
-	/**
-	 * @param rue the rue to set
-	 */
-	public void setRue(String rue) {
-		this.rue = rue;
-	}
-	/**
-	 * @return the cp
-	 */
-	public int getCp() {
-		return cp;
-	}
-	/**
-	 * @param cp the cp to set
-	 */
-	public void setCp(int cp) {
-		this.cp = cp;
-	}
-	/**
-	 * @return the ville
-	 */
-	public String getVille() {
-		return ville;
-	}
-	/**
-	 * @param ville the ville to set
-	 */
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
-	/**
-	 * @return the password
-	 */
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public int getCp() {
+        return cp;
+    }
+
+    public void setCp(int cp) {
+        this.cp = cp;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public int getAdministrateur() {
+        return administrateur;
+    }
+
+    public void setAdministrateur(int administrateur) {
+        this.administrateur = administrateur;
+    }
+
 	public String getPassword() {
 		return password;
 	}
-	/**
-	 * @param password the password to set
-	 */
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	/**
-	 * @return the credit
-	 */
-	public int getCredit() {
-		return credit;
-	}
-	/**
-	 * @param credit the credit to set
-	 */
-	public void setCredit(int credit) {
-		this.credit = credit;
-	}
-	/**
-	 * @return the administrateur
-	 */
-	public int getAdministrateur() {
-		return administrateur;
-	}
-	/**
-	 * @param administrateur the administrateur to set
-	 */
-	public void setAdministrateur(int administrateur) {
-		this.administrateur = administrateur;
-	}
 }
+
+
